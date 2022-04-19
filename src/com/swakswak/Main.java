b6ba842669819161a8035f6b8ffd0d374499539b
swakswak <hschoi0702@gmail.com>
2022-04-19 20:36:32 +0900

    Gradle 추가로 인한 프로젝트 디렉터리 구조 변경

--- src/com/swakswak/Main.java
+++
@@ -1,8 +0,0 @@
-package com.swakswak;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/test/java/com/swakswak/MainTest.java
@@ -0,0 +1,7 @@
+package com.swakswak;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+}
