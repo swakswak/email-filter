a2f287ee6738c1964a7db08cc2e10ae53bf6b600
swakswak <hschoi0702@gmail.com>
2022-04-21 18:33:50 +0900

    MailBoxHolder Singleton으로 변경

--- src/main/java/com/swakswak/mailbox/MailBoxHolder.java
+++ src/main/java/com/swakswak/mailbox/MailBoxHolder.java
@@ -1,15 +1,25 @@
 package com.swakswak.mailbox;
 
 import java.util.EnumMap;
+import java.util.Map;
 
 /**
  * @author hyoseok choi (hschoi0702@gmail.com)
  **/
-class MailBoxHolder {
-    private final EnumMap<MailType, MailBox> mailBoxMap;
+public class MailBoxHolder {
+    private final Map<MailType, MailBox> mailBoxMap;
+    private static MailBoxHolder instance;
 
-    MailBoxHolder(EnumMap<MailType, MailBox> mailBoxMap) {
-        this.mailBoxMap = mailBoxMap;
+    MailBoxHolder() {
+        this.mailBoxMap = new EnumMap<>(MailType.class);
+    }
+
+    public static synchronized MailBoxHolder getInstance() {
+        if (instance == null) {
+            return new MailBoxHolder();
+        }
+
+        return instance;
     }
 
     public void add(MailType key, MailBox value) {
